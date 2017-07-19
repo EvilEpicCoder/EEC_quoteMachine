d29deb062071593c3a081b9e0ea9bbac265ef1a5
Igor Gavelyuk <1st.elixile@gmail.com>
2017-07-19 16:40:07 +0300

    added animation, it was hard! damn!

--- README.md
+++ README.md
@@ -2,17 +2,18 @@
 ***
 ## Free Code Camp Practical Task "Quote Machine"
 
-
+> So I like games, I like games with style and gameplay, "Disciples" is an old game, the amazing old game.
 > In the memory of great game "Disciples Dark Prophecy"
 
 About my Quote Machine:
   * Stylish app, that will give you an "Disciples" atmosphere, also app had cool "dark" soundtrack from the game.
-How I see my app:
+
+ How I see my app:
 1. Dark background, cool "devil" slide effect, three buttons in "old games" style, random quotes, tweeter, get a quote.
 2. Background music
 3. Strange quotes, I will try to do them long smart and completely random.
 
-Features to add:
+ Features to add:
 1. Add cool slide effect
 2. Remove some minor bugs
 3. Improove quotes structure
@@ -20,5 +21,5 @@ Features to add:
 How to use:
   * Download as ZIP, unpack, run EEC_quoteMachine.html
 
-> So I like games, I like games with style and gameplay, "Disciples" is an old game, the amazing old game.
+
 
--- qm.js
+++ qm.js
@@ -7,11 +7,16 @@ var qAuthor=[];
 var qm_text=0;
 var qm_author=0;
 var qm_picture=0;
+var qm_rotate=0;
+var qm_flip=0;
 var prev=0;
 var tweet=0;
 var next=0;
 var magik=0;
 var myVal=0;
+var boolActive=false;
+//debug
+var interations=0;
 
 window.onload=function(){
 
@@ -24,6 +29,8 @@ qAuthor=[" Evil "," Lord "," Epic "," Coder "," Azazel "," Gabriel "," Uther ", 
 qm_text=document.getElementById("qmtext");
 qm_author=document.getElementById("qmauthor");
 qm_picture=document.getElementById("one");
+qm_rotate=document.getElementById("rotate");
+qm_flip=document.getElementById("flip");
 prev=document.getElementsByClassName("prev");
 tweet=document.getElementsByClassName("social");
 next=document.getElementById("next");
@@ -32,25 +39,57 @@ randomWorld();
 	
 }
 function randomWorld(){
+interations++;
+console.log(interations);
 magik=Math.floor(Math.random() * (11 - 0)) + 0;
 qm_picture.style.backgroundImage = "url('pictures_qm/"+magik+".jpg')";
 qm_text.textContent=qArray1[Math.floor(Math.random() * (8 - 0)) + 0]+qArray2[Math.floor(Math.random() * (3 - 0)) + 0]+qArray3[Math.floor(Math.random() * (5 - 0)) + 0]+qArray4[Math.floor(Math.random() * (6 - 0)) + 0];	
-qm_author.textContent=qAuthor[Math.floor(Math.random() * (8 - 0)) + 0]+qAuthor[Math.floor(Math.random() * (8 - 0)) + 0];	
+qm_author.textContent=qAuthor[Math.floor(Math.random() * (8 - 0)) + 0]+qAuthor[Math.floor(Math.random() * (8 - 0)) + 0];
+	//qm_picture.className = "dsp1";
+	//if(qm_rotate.className =='rotate1'|| qm_flip.className =='flip1')
+	//{
+		var xBug=setInterval(function(){
+		reMove();
+		console.log(xBug);
+		}, 2000);
+	if(xBug!="undefined"){
+		qm_rotate.classList.remove('rotate');
+		qm_flip.classList.remove('flip');
+		qm_rotate.classList.add("rotate1");
+		qm_flip.classList.add("flip1");
+	}
+	
+	console.log(qm_rotate.classList+"Here");
+	console.log(qm_flip.classList+"Here");
+	
+}
+function reMove(){
+	qm_rotate.classList.remove('rotate1');
+	qm_flip.classList.remove('flip1');
+	qm_rotate.classList.add("rotate");
+	qm_flip.classList.add("flip");
+	clearInterval(xBug);
 }
 function checkButtons(evt){
 	var x=evt.target.innerText;
 	console.log(myVal);
 	if(x=='Get random!'){
 	clearInterval(myVal);
+	
+	qm_rotate.classList.remove('rotate');
+	qm_flip.classList.remove('flip');
+	//console.log(qm_rotate.classList+"removed");
+	//console.log(qm_flip.classList+"removed");
+	
 	randomWorld();
 	}else if(x=="Tweet quote"){
 	clearInterval(myVal);
 	//location.href = "https://twitter.com/intent/tweet?text=HelloWorld";
 	//location.href = "https://twitter.com/intent/tweet?text="+"Check out my quote mashine!\n"+qm_text.textContent+"\n https://codepen.io/EvilEpicCoder/pen/dRZYBP \n";
 	window.open("https://twitter.com/intent/tweet?text="+"Check out my quote mashine! \n "+qm_text.textContent+" \n https://codepen.io/EvilEpicCoder/pen/dRZYBP \n ",'_blank');
-	}else if(x=="Auto slide 5sec"){
+	}else if(x=="Auto slide 10sec"){
 		if(myVal==0){
-		myVal=setInterval(function(){ randomWorld(); }, 5000);
+		myVal=setInterval(function(){ randomWorld();randomWorld(); }, 10000);
 		}else{
 		clearInterval(myVal);
 		myVal=0;
